dc18c4d5b8e11ac16f2abf6865072d4a2961d9e8
Weyman Wong <wxwong806@gmail.com>
2024-03-29 10:40:29 +1300

    Changed loading state to be of type boolean

--- src/app/components/user-auth-form/auth-form.jsx
+++ src/app/components/user-auth-form/auth-form.jsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 const AuthForm = ({ register = false, action }) => {
   // Error for the form
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const submitForm = async (e) => {
     e.preventDefault(); // Stops reload
