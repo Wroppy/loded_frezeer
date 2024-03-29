f2782af8074b5f450b9a64f21faeb2ec1e643cd0
Weyman Wong <wxwong806@gmail.com>
2024-03-29 20:58:46 +1300

    Disabled the navbar drawer button if the user isn't signed in

--- src/app/components/navbar/drawer/vertical-nav.jsx
+++ src/app/components/navbar/drawer/vertical-nav.jsx
@@ -8,13 +8,14 @@ import { useState } from "react";
 import DrawerHeader from "@/app/components/navbar/drawer/drawer-header";
 import DrawerLinks from "@/app/components/navbar/drawer/drawer-links";
 import DrawerFooter from "@/app/components/navbar/drawer/drawer-footer";
+import { useSession } from "next-auth/react";
 
 const VerticalNav = () => {
   const [open, setOpen] = useState(false);
+  const { data: session } = useSession();
 
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
-
   return (
     <>
       <Drawer anchor="left" open={open} onClose={closeDrawer}>
@@ -24,7 +25,7 @@ const VerticalNav = () => {
           <DrawerFooter />
         </div>
       </Drawer>
-      <Button onClick={openDrawer} className={styles.drawerButton}>
+      <Button disabled={!session}  onClick={openDrawer} className={styles.drawerButton}>
         <Menu />
       </Button>
     </>
