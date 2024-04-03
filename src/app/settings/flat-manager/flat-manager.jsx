caaccb1bac43f247269f6a31b647e631214b62c2
Weyman Wong <wxwong806@gmail.com>
2024-04-04 01:13:26 +1300

    Added a create flat component

--- src/app/settings/flat-manager/flat-manager.jsx
+++ src/app/settings/flat-manager/flat-manager.jsx
@@ -1,12 +1,13 @@
 import { Paper } from "@mui/material";
 import styles from "@/app/settings/settings.module.css";
-import FlatSummary from "./flat-summary/flat-summary";
+import FlatSummary from "@/app/settings/flat-manager/flat-summary/flat-summary";
+import NoFlat from "@/app/settings/flat-manager/no-flat";
 
 const FlatManager = () => {
   return (
     <div className={styles.flatManager}>
       <h3>Manage your flat:</h3>
-      <FlatSummary />
+      <NoFlat />
     </div>
   );
 };
---
+++ src/app/settings/flat-manager/no-flat.jsx
@@ -0,0 +1,20 @@
+import { CircularProgress, Paper, TextField } from "@mui/material";
+import styles from "@/app/settings/settings.module.css";
+import CreateFlatForm from "@/app/settings/flat-manager/create-flat-form";
+
+const NoFlat = () => {
+  return (
+    <>
+      {" "}
+      <Paper className={styles.flatName}>
+        <span className={styles.flatSetUpText}>
+          You have not set up a flat yet. Create a new one below.
+        </span>
+      </Paper>
+      {/* Form for creating a new flat */}
+      <CreateFlatForm />
+    </>
+  );
+};
+
+export default NoFlat;
