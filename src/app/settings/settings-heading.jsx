d4bff6a17ced341eed9a4fde47e6330ae07970cb
Weyman Wong <wxwong806@gmail.com>
2024-04-04 13:30:54 +1300

    Fixed hydration error with the settings header

--- src/app/settings/settings-heading.jsx
+++ src/app/settings/settings-heading.jsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { cookieGetter } from "@/app/utils/cookieGetter";
-
+import { useEffect, useState } from "react";
 
 const SettingsHeading = () => {
   const username = cookieGetter("username");
+
+  // Deals with the hydration error 
+  const [isClient, setIsClient] = useState(false)
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+ 
+
   return ( <div>
     <h3>
-      Welcome {username}!
+      Welcome {isClient && username}!
     </h3>
   </div> );
 }
--- src/app/utils/cookieGetter.js
+++ src/app/utils/cookieGetter.js
@@ -2,12 +2,16 @@
 
 // Returns the value of the parsed cookie
 export const cookieGetter = (value) => {
-  const cookies = document.cookie.split(';');
+  try {
+    const cookies = document.cookie.split(";");
 
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].split('=');
-    if (cookie[0].trim() === value) {
-      return cookie[1];
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].split("=");
+      if (cookie[0].trim() === value) {
+        return cookie[1];
+      }
     }
+  } catch (error) {
+    return null;
   }
-}
+};
