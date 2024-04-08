ef2694eb8a2809ce12b171dfbc0ac15db10ea718
Weyman Wong <wxwong806@gmail.com>
2024-04-09 11:27:27 +1200

    Implemeneted logout and settings button link

--- src/app/components/navbar/drawer/drawer-footer.jsx
+++ src/app/components/navbar/drawer/drawer-footer.jsx
@@ -4,17 +4,31 @@ import styles from "@/app/components/navbar/drawer/drawer.module.css";
 import { Settings } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { useRouter } from "next/navigation";
 
 const DrawerFooter = () => {
+  const router = useRouter();
 
   const logout = async () => {
+    // Remove token from cookies
+    document.cookie.split(";").forEach((c) => {
+      document.cookie = c
+        .replace(/^ +/, "")
+        .replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`);
+    });
+
     // Redirect to login page
-    // window.location.href = "/login";
+    window.location.href = "/login";
   }
+
+  const settings = () => {
+    router.push("/settings");
+  }
+
 
   return (
     <div className={styles.navFooter}>
-      <IconButton>
+      <IconButton onClick={settings}>
         <Settings />
       </IconButton>
       <IconButton onClick={logout} className={`${styles.flip}`}>
