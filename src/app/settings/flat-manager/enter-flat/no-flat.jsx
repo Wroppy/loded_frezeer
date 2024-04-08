57e4f204260292f349f31547502457129b033456
Weyman Wong <wxwong806@gmail.com>
2024-04-08 23:31:49 +1200

    Added styles to accomodate for smaller screen sizes

--- src/app/settings/flat-manager/enter-flat/no-flat.jsx
+++ src/app/settings/flat-manager/enter-flat/no-flat.jsx
@@ -5,9 +5,13 @@ import EnterFlat from "./enter-flat";
 const NoFlat = () => {
   return (
     <>
-     <Paper className={styles.flatName}>
+      <Paper className={styles.flatName}>
         <span className={styles.flatSetUpText}>
           You have not set up a flat yet. Create a new one or join one below.
+        </span>
+        <br/>
+        <span className={styles.flatSetUpText}>
+          Beware! If you join a new flat, you will not be able to leave!
         </span>
       </Paper>
       {/* Form for creating or joining a new flat */}
