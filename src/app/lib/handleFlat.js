9655ea9b6130560829ab17fb68173b615e8bfd06
Weyman Wong <wxwong806@gmail.com>
2024-04-23 00:49:08 +1200

    Added a connectToDb function call to stop timing out

--- src/app/lib/handleFlat.js
+++ src/app/lib/handleFlat.js
@@ -1,9 +1,12 @@
 import { User, Flat } from "@/app/lib/models";
 import { getFlatModel } from "@/app/lib/modelBlueprints";
 import { jwtVerify } from "@/app/lib/utils";
+import connectToDb from "@/app/lib/connectToDb";
 
 export const createFlat = async (userId, name) => {
   try {
+    await connectToDb();
+
     const flatModel = getFlatModel(userId, name);
     const newFlat = new Flat(flatModel);
     const flat = await Flat.create(newFlat);
@@ -18,6 +21,8 @@ export const createFlat = async (userId, name) => {
 // Used to find if a user is in a flat
 export const isUserInFlat = async (encryptedId) => {
   try {
+    await connectToDb();
+
     const userId = jwtVerify(encryptedId);
 
     // Finds all flats that the user is in
@@ -36,9 +41,11 @@ export const isUserInFlat = async (encryptedId) => {
 
 export const joinFlat = async (userId, joinId) => {
   try {
+    await connectToDb();
+
     const flat = await Flat.findOne({
       joinId,
-    }); 
+    });
 
     if (!flat) {
       return { success: false, error: "Invalid join code" };
@@ -49,13 +56,18 @@ export const joinFlat = async (userId, joinId) => {
 
     return { success: true };
   } catch (error) {
-    return { success: false, error: "An error occurred while joining the flat" };
+    return {
+      success: false,
+      error: "An error occurred while joining the flat",
+    };
   }
-}
+};
 
 // Returns data for the settings page
 export const getFlatSettings = async (userId) => {
   try {
+    await connectToDb();
+
     const flatData = await Flat.findOne({
       tenants: userId,
     });
@@ -70,14 +82,17 @@ export const getFlatSettings = async (userId) => {
       user = await User.findOne({ userId: tenantId });
       tenants.push(user.name);
     }
-    
+
     const flat = {
       flatName: flatData.name,
       joinCode: flatData.joinId,
       tenants,
-    }
+    };
     return { success: true, flat };
   } catch (error) {
-    return { success: false, error: "An error occurred while getting flat settings" };
+    return {
+      success: false,
+      error: "An error occurred while getting flat settings",
+    };
   }
-}
+};
